e7461fa5ead124331d50fd9c65676ef8667f3035
Brian Grant <briankgrant@gmail.com>
2025-10-07 06:39:32 -0700

    Setup scripts

---
+++ setup-flux.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Add the following to /etc/hosts
+# 127.0.0.1	dev.appchat.cubby.bz
+# 127.0.0.1	www.appchat.cubby.bz
+# 127.0.0.1	dev-vote.appvote.cubby.bz
+# 127.0.0.1	dev-results.appvote.cubby.bz
+# 127.0.0.1	www.appvote.cubby.bz
+# 127.0.0.1	results.appvote.cubby.bz
+# 127.0.0.1	dev.apptique.cubby.bz
+# 127.0.0.1	www.apptique.cubby.bz
+
+cd ..
+
+kind create cluster --name dev --config setup/dev-cluster.yaml 
+#https://kind.sigs.k8s.io/docs/user/ingress
+#kubectl apply -f ../deploy-ingress-nginx.yaml
+kubectl apply -f https://kind.sigs.k8s.io/examples/ingress/deploy-ingress-nginx.yaml
+kubectl create namespace appchat
+kubectl create namespace appvote
+kubectl create namespace apptique
+kubectl apply -f appchat/flux/gitrepository.yaml
+kubectl apply -f appchat/flux/helmrelease-dev.yaml
+kubectl apply -f appvote/flux/gitrepository.yaml
+kubectl apply -f appvote/flux/helmrelease-dev.yaml
+kubectl apply -f apptique/flux/gitrepository.yaml
+kubectl apply -f apptique/flux/helmrelease-dev.yaml
+
+kind create cluster --name prod --config setup/prod-cluster.yaml 
+#kubectl apply -f ../deploy-ingress-nginx.yaml
+kubectl apply -f https://kind.sigs.k8s.io/examples/ingress/deploy-ingress-nginx.yaml
+kubectl create namespace appchat
+kubectl create namespace appvote
+kubectl create namespace apptique
+kubectl apply -f appchat/flux/gitrepository.yaml
+kubectl apply -f appchat/flux/helmrelease-prod.yaml
+kubectl apply -f appvote/flux/gitrepository.yaml
+kubectl apply -f appvote/flux/helmrelease-prod.yaml
+kubectl apply -f apptique/flux/gitrepository.yaml
+kubectl apply -f apptique/flux/helmrelease-prod.yaml
---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Deprecated. Use setup-flux.sh
+cd ..
+kind create cluster --name dev --config setup/dev-cluster.yaml 
+kubectl apply -f ../deploy-ingress-nginx.yaml
+kubectl create namespace appchat
+helm install appchat appchat --values appchat/values.yaml --values appchat/values-dev.yaml -n appchat
+kubectl create namespace appvote
+helm install appvote appvote --values appvote/values.yaml -n appvote
+kubectl create namespace apptique
+helm install apptique aptique/helm-chart --values apptique/helm-chart/values.yaml --values apptique/helm-chart/values-dev.yaml -n apptique
